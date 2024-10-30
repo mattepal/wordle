d5dd7dbe997e624e17cdf9056baeda7febd8bd6a
Matteo Paletti <mpaletti04@gmail.com>
2024-10-30 12:02:33 +0100

    change

---
+++ src/main/java/com/example/wordle/Game.java
@@ -0,0 +1,9 @@
+package com.example.wordle;
+
+public class Game {
+    int a=0;
+
+    public Game(int a) {
+        this.a = a;
+    }
+}
---
+++ src/main/java/com/example/wordle/Word.java
@@ -0,0 +1,17 @@
+package com.example.wordle;
+
+public class Word {
+    String word;
+
+    public Word(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
