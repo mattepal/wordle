694041d5adce83b08c5a8313c7382f56f0050bdc
Simone Gaglione <317198@studenti.unimore.it>
2024-10-31 18:44:31 +0100

    matteoooooo

--- src/main/java/com/example/wordle/Controller.java
+++ src/main/java/com/example/wordle/Controller.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 public class Controller {
     @FXML
     private TextField guessInput;
+    // riga 0
     @FXML
     private Label box00 = new Label();
     @FXML
@@ -22,23 +23,87 @@ public class Controller {
     private Label box03 = new Label();
     @FXML
     private Label box04 = new Label();
+    // riga 1
+    @FXML
+    private Label box10 = new Label();
+    @FXML
+    private Label box11 = new Label();
+    @FXML
+    private Label box12 = new Label();
+    @FXML
+    private Label box13 = new Label();
+    @FXML
+    private Label box14 = new Label();
+    // riga 2
+    @FXML
+    private Label box20 = new Label();
+    @FXML
+    private Label box21 = new Label();
+    @FXML
+    private Label box22 = new Label();
+    @FXML
+    private Label box23 = new Label();
+    @FXML
+    private Label box24 = new Label();
+    // riga 3
+    @FXML
+    private Label box30 = new Label();
+    @FXML
+    private Label box31 = new Label();
+    @FXML
+    private Label box32 = new Label();
+    @FXML
+    private Label box33 = new Label();
+    @FXML
+    private Label box34 = new Label();
+    // riga 4
+    @FXML
+    private Label box40 = new Label();
+    @FXML
+    private Label box41 = new Label();
+    @FXML
+    private Label box42 = new Label();
+    @FXML
+    private Label box43 = new Label();
+    @FXML
+    private Label box44 = new Label();
+    // riga 5
+    @FXML
+    private Label box50 = new Label();
+    @FXML
+    private Label box51 = new Label();
+    @FXML
+    private Label box52 = new Label();
+    @FXML
+    private Label box53 = new Label();
+    @FXML
+    private Label box54 = new Label();
+
     @FXML
     private Label DebugLabel;
 
     private Player player;
     private Word word;
+    int row;
 
     public void initialize() {
         player = new Player();
         word = new Word();
         DebugLabel.setText("Write here a word");
-
+        row = 0;
     }
 
     //Button click event
     @FXML
     protected void checkGuess() throws IOException {
         Label[] row = {box00, box01, box02, box03, box04};
+        Label[][] matrix = {
+                {box00, box01, box02, box03, box04},
+                {box10, box11, box12, box13, box14},
+                {box20, box21, box22, box23, box24},
+                {box30, box31, box32, box33, box34},
+                {box40, box41, box42, box43, box44},
+                {box50, box51, box52, box53, box54}};
         String guess = guessInput.getText();
         if(!player.isOutOfIndex()){
             if(guess.length() != word.getWord().length()){
