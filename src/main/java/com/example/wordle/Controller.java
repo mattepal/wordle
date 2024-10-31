c7ce90acdec275424d8b8a588849f787c14b075c
Simone Gaglione <317198@studenti.unimore.it>
2024-10-31 11:13:15 +0100

    change2

--- src/main/java/com/example/wordle/Controller.java
+++ src/main/java/com/example/wordle/Controller.java
@@ -2,8 +2,10 @@ package com.example.wordle;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
+    public TextField guessInput;
     @FXML
     private Label welcomeText;
 
