9c9754638c6198c7491992088ad58de2b619eb0c
Matteo Paletti <mpaletti04@gmail.com>
2024-11-01 23:24:03 +0100

    change

--- src/main/java/com/example/wordle/model/Attempt.java
+++ src/main/java/com/example/wordle/model/Attempt.java
@@ -1,8 +1,8 @@
 package com.example.wordle.model;
 
 public class Attempt {
-    private String guess;
-    private String result;
+    private final String guess;
+    private final String result;
 
     public Attempt(String guess, String word) {
         this.guess = guess;
@@ -11,13 +11,26 @@ public class Attempt {
 
     private String checkGuess(String word) {
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < guess.length(); i++) {
+        int[] letterCount=new int[26];
+        for (char c: word.toCharArray()){
+            letterCount[c-'a']++;
+        }
+        for (int i = 0; i <guess.length(); i++) {
             if (guess.charAt(i) == word.charAt(i)) {
                 result.append("G");
-            } else if (word.contains(String.valueOf(guess.charAt(i)))) {
-                result.append("Y");
-            } else {
-                result.append("R");
+                letterCount[guess.charAt(i)-'a']--;
+            } else{
+                result.append(" ");
+            }
+        }
+        for (int i = 0; i < guess.length(); i++) {
+            if (result.charAt(i)==' ') {
+                if (word.contains(String.valueOf(guess.charAt(i))) && letterCount[guess.charAt(i)-'a']>0) {
+                    result.setCharAt(i,'Y');
+                    letterCount[guess.charAt(i)-'a']--;
+                } else {
+                    result.setCharAt(i,'R');
+                }
             }
         }
         return result.toString();
